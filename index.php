56516524a720ae1668cb96e1169a21c99ddf0141
Xaviergarces <Xaviergarces@VWW-LT08.spiritmarketing.local>
2013-05-15 13:05:32 +0100

    Second Upload

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+
+
+
+
 if (version_compare(PHP_VERSION, '5.3.1', '<'))
 {
 	die('Your host needs to use PHP 5.3.1 or higher to run this version of Joomla!');
